751e4b000596aa1a4ad27bdef948408d164700ba
Alamgir Khan <alamgir.inu96@gmail.com>
2026-05-03 10:35:44 +0300

    fix: ordersound

--- app/dashboard/orders/page.js
+++ app/dashboard/orders/page.js
@@ -1,14 +1,14 @@
 "use client";
 
 import { useState, useEffect, useRef, useCallback } from "react";
-// import  {useOrderSound } from "@/components/OrderSound";
+import  {useOrderSound } from "@/components/OrderSound";
 
 export default function OrdersPage() {
   const [orders, setOrders] = useState([]);
   const [loading, setLoading] = useState(true);
   const [lastCount, setLastCount] = useState(null);
   const [newOrderFlash, setNewOrderFlash] = useState(false);
-  // const { playAlert } = useOrderSound();
+  const { playAlert } = useOrderSound();
   const isFirstLoad = useRef(true);
 
   const fetchOrders = useCallback(async () => {
